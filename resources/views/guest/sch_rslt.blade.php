4501bcadfadf71551476d156b28b491a1bdbe07b
booyah35 <kouya0807kouya@gmail.com>
2022-11-27 21:56:04 +0900

    guest search conditions changed

--- resources/views/guest/sch_rslt.blade.php
+++ resources/views/guest/sch_rslt.blade.php
@@ -2,6 +2,7 @@
     <div class="py-1">
         <h1 class="text-center text-xl">検索結果</h1>
     </div>
+    
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-cyan-100 dark:bg-gray-700 dark:text-gray-400">
